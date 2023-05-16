ff58acb22d4e8239d08ec5421e22debbc53682ae
Brian Ketelsen <bketelsen@gmail.com>
2023-05-16 09:40:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Devbox Fleek Proof of Concept
+
+This is a proof of concept for a devbox global shell configuration that is pretty close to the Fleek `HIGH` bling configuration.
+
+## Installation
+
+### Install Devbox
+
+```bash
+curl -fsSL https://get.jetpack.io/devbox | bash
+```
+
+### Clone this repo into the Devbox global directory
+
+```bash
+git clone https://github.com/bketelsen/devboxfleek.git ~/.local/share/devbox/global/default
+```
+
+### Source the Devbox global configuration
+
+See directions [here][def]
+
+[def]: https://www.jetpack.io/devbox/docs/devbox_global/#add-global-packages-to-your-current-host-shell
---
+++ init.sh
@@ -0,0 +1,23 @@
+shell=$(basename $SHELL)
+SCRIPT_PATH=`dirname "$0"`; SCRIPT_PATH=`eval "cd \"$SCRIPT_PATH\" && pwd"`
+
+case $shell in
+     "zsh" )
+           if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "vterm") ]]; then
+                eval "$(starship init zsh)"
+           fi
+           eval "$(direnv hook zsh)"
+           if [[ $options[zle] = on ]]; then
+                eval "$(atuin init zsh )"
+           fi
+           ;;
+     "bash" )
+           eval "$(starship init bash)"
+           [[ -f "${SCRIPT_PATH}/bash-preexec.sh" ]] && source "${SCRIPT_PATH}/bash-preexec.sh"
+           eval "$(atuin init bash)"
+           eval "$(direnv hook bash)"
+           ;;
+     * )
+           echo "Unsupported shell: '$shell'"
+           ;;
+esac
